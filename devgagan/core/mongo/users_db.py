a3003458ea9012e0d3d038e3904fe89413e30fc5
Pushpendra <Pkr99153@gmail.com>
2026-01-14 21:38:33 +0530

    users_db.py

--- devgagan/core/mongo/users_db.py
+++ devgagan/core/mongo/users_db.py
@@ -1,36 +1,32 @@
 # ---------------------------------------------------
 # File Name: users_db.py
-# Description: MongoDB user management
+# MongoDB based user storage (FINAL CLEAN VERSION)
 # ---------------------------------------------------
 
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from motor.motor_asyncio import AsyncIOMotorClient
 from devgagan.config import MONGO_DB
 
+MongoCli = AsyncIOMotorClient
+
 mongo = MongoCli(MONGO_DB)
-db = mongo.users_db
+db = mongo.users.users_db
 
 
 async def get_users():
-    user_list = []
-    async for user in db.users.find({"user": {"$gt": 0}}):
-        user_list.append(user["user"])
-    return user_list
+    users = []
+    async for doc in db.find({}, {"_id": 0, "user": 1}):
+        users.append(doc["user"])
+    return users
 
 
-async def get_user(user):
-    users = await get_users()
-    return user in users
+async def get_user(user: int) -> bool:
+    return await db.find_one({"user": user}) is not None
 
 
-async def add_user(user):
-    users = await get_users()
-    if user in users:
-        return
-    await db.users.insert_one({"user": user})
+async def add_user(user: int):
+    if not await get_user(user):
+        await db.insert_one({"user": user})
 
 
-async def del_user(user):
-    users = await get_users()
-    if user not in users:
-        return
-    await db.users.delete_one({"user": user})
+async def del_user(user: int):
+    await db.delete_one({"user": user})
