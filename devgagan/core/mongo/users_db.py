c74da028110d94e0fb26ed92afbce0e8f0c9a85f
Pushpendra <Pkr99153@gmail.com>
2026-01-14 21:58:42 +0530

    users_db.py

--- devgagan/core/mongo/users_db.py
+++ devgagan/core/mongo/users_db.py
@@ -1,18 +1,11 @@
-# ---------------------------------------------------
-# File Name: users_db.py
-# MongoDB based user storage (FINAL CLEAN VERSION)
-# ---------------------------------------------------
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from devgagan.config import MONGO_DB
 
-# MongoDB Client Setup
 client = AsyncIOMotorClient(MONGO_DB)
 db = client["devgagan"]["users"]
 
 
 async def get_users():
-    """Get all user IDs from database"""
     users = []
     async for doc in db.find({}, {"_id": 0, "user": 1}):
         if "user" in doc:
@@ -21,18 +14,15 @@ async def get_users():
 
 
 async def get_user(user: int) -> bool:
-    """Check if user exists in database"""
     found = await db.find_one({"user": user})
     return found is not None
 
 
 async def add_user(user: int):
-    """Add new user to database"""
     exists = await get_user(user)
     if not exists:
         await db.insert_one({"user": user})
 
 
 async def del_user(user: int):
-    """Delete user from database"""
     await db.delete_one({"user": user})
